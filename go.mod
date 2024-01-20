2bdcf5e19cabb9c949faf365c20f9783cbe04405
enophan <zhaooooooyifan@qq.com>
2024-01-20 22:37:31 +0800

    实现基本方法与结构

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module hutuodb
 
 go 1.21.5
+
+require github.com/google/btree v1.1.2
---
+++ index/btree.go
@@ -0,0 +1,48 @@
+package index
+
+import (
+	"hutuodb/data"
+	"sync"
+
+	"github.com/google/btree"
+)
+
+// Btree 索引
+type Btree struct {
+	tree *btree.BTree
+	lock *sync.RWMutex // 并发要自己上锁
+}
+
+func (b *Btree) Put(key []byte, pos *data.LogRecordPos) bool {
+	item := Item{
+		key: key,
+		pos: pos,
+	}
+	b.lock.Lock()
+	b.tree.ReplaceOrInsert(&item)
+	b.lock.Unlock()
+	return true
+}
+func (b *Btree) Get(key []byte) *data.LogRecordPos {
+	item := &Item{
+		key: key,
+	}
+	bitem := b.tree.Get(item)
+	if bitem == nil {
+		return nil
+	}
+
+	return bitem.(*Item).pos
+}
+func (b *Btree) Delete(key []byte) bool {
+	item := &Item{
+		key: key,
+	}
+	b.lock.Lock()
+	bitem := b.tree.Delete(item)
+	if bitem == nil {
+		return false
+	}
+	b.lock.Unlock()
+	return true
+}
