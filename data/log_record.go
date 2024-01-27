f60779bf709e4602a80ba9f091c129efdb83eeca
enophan <zhaooooooyifan@qq.com>
2024-01-27 09:56:32 +0800

    chore

--- data/log_record.go
+++ data/log_record.go
@@ -1,8 +1,8 @@
 package data
 
 type LogRecordPos struct {
-	// 文件名
+	// 文件名 int64 可能比较大，比较浪费 int32 比较合理
 	Fid uint32
-	// 值的偏移位置
+	// 存储值在这一条目中的偏移位置
 	Offset int64
 }
--- fio/file_io_test.go
+++ fio/file_io_test.go
@@ -1,10 +1,15 @@
 package fio
 
 import (
+	"path/filepath"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 // 测试
 func TestNewFileIOManager(t *testing.T) {
-
+	fio, err := NewFileIOManager(filepath.Join("/tmp/bitcask", "go.data"))
+	assert.Nil(t, err)
+	assert.NotNil(t, fio)
 }
