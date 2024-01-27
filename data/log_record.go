091c7f629d1d849d9d850b078cd76d8acbae0a54
enophan <zhaooooooyifan@qq.com>
2024-01-27 22:34:41 +0800

    chore

--- data/log_record.go
+++ data/log_record.go
@@ -1,8 +1,20 @@
 package data
 
 type LogRecordPos struct {
-	// 文件名 int64 可能比较大，比较浪费 int32 比较合理
-	Fid uint32
-	// 存储值在这一条目中的偏移位置
-	Offset int64
+	Fid    uint32 // 文件名 int64 可能比较大，比较浪费 int32 比较合理
+	Offset int64  // 存储值在这一条目中的偏移位置
+}
+
+type LogRecordType = byte
+
+const (
+	LogRecordNormal LogRecordType = iota
+	LogRecordDelete
+)
+
+// LogRecord 写入到数据文件的记录
+type LogRecord struct {
+	Key   []byte
+	Value []byte
+	Type  LogRecordType // 这条数据是删除还是存储？
 }
