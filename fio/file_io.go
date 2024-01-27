8cf9d812af1115b81a36fdda45bccf1298e16b7c
enophan <zhaooooooyifan@qq.com>
2024-01-27 22:35:34 +0800

    test: 磁盘读写等测试

--- fio/file_io.go
+++ fio/file_io.go
@@ -7,6 +7,7 @@ type FileIO struct {
 	fd *os.File
 }
 
+// NewFileIOManager 创建一新 FileIO（实现了IOManager接口）
 func NewFileIOManager(fileName string) (*FileIO, error) {
 	fd, err := os.OpenFile(
 		fileName,
@@ -20,18 +21,24 @@ func NewFileIOManager(fileName string) (*FileIO, error) {
 	return &FileIO{fd: fd}, nil
 }
 
+// 实现 IOManager 接口
+
+// Read 从哪个位置（offset）读多长（b）的数据，返回读出的数据长度和 err
 func (fio *FileIO) Read(b []byte, offset int64) (int, error) {
 	return fio.fd.ReadAt(b, offset)
 }
 
+// Write 写入多长数据（b），返回这次写入的数据长度和 err
 func (fio *FileIO) Write(b []byte) (int, error) {
 	return fio.fd.Write(b)
 }
 
+// Sync
 func (fio *FileIO) Sync() error {
 	return fio.fd.Sync()
 }
 
+// Close
 func (fio *FileIO) Close() error {
 	return fio.fd.Close()
 }
--- fio/file_io_test.go
+++ fio/file_io_test.go
@@ -1,15 +1,60 @@
 package fio
 
 import (
+	"os"
 	"path/filepath"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
+// 清除临时文件
+func destroyFile(fileName string) {
+	if err := os.RemoveAll(fileName); err != nil {
+		panic(err)
+	}
+}
+
 // 测试
 func TestNewFileIOManager(t *testing.T) {
-	fio, err := NewFileIOManager(filepath.Join("/tmp/bitcask", "go.data"))
+	fileName := filepath.Join("/tmp", "test.data")
+	fio, err := NewFileIOManager(fileName)
+	defer destroyFile(fileName)
+
 	assert.Nil(t, err)
 	assert.NotNil(t, fio)
 }
+
+func TestRead(t *testing.T) {
+	fileName := filepath.Join("/tmp", "test.data")
+	fio, err := NewFileIOManager(fileName)
+	defer destroyFile(fileName)
+	assert.Nil(t, err)
+	assert.NotNil(t, fio)
+
+	_, err = fio.Write([]byte("0123456789"))
+	assert.Nil(t, err)
+
+	b := make([]byte, 4)
+	l, err := fio.Read(b, 0)
+	assert.Nil(t, err)
+	assert.Equal(t, l, 4)
+}
+
+func TestWrite(t *testing.T) {
+	fileName := filepath.Join("/tmp", "test.data")
+	fio, err := NewFileIOManager(fileName)
+	defer destroyFile(fileName)
+	assert.Nil(t, err)
+	assert.NotNil(t, fio)
+
+	_, err = fio.Write([]byte("writesdfghjkl"))
+	assert.Nil(t, err)
+
+	_, err = fio.Write([]byte(""))
+	assert.Nil(t, err)
+}
+
+func TestSync(t *testing.T) {}
+
+func TestClose(t *testing.T) {}
