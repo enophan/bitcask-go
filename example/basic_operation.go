4c3c66f4e66c5d8438377612c4cd23f411badf4a
enophan <zhaooooooyifan@qq.com>
2024-02-02 10:30:18 +0800

    feat：增加实例

---
+++ example/basic_operation.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	bitcask "bitcask"
+	"fmt"
+)
+
+func main() {
+	opts := bitcask.DefaultOptions
+	opts.DirPath = "/tmp/bitcask-go"
+
+	db, err := bitcask.Open(opts)
+	if err != nil {
+		panic(err)
+	}
+
+	// err = db.Put([]byte("name"), []byte("enophan"))
+	// if err != nil {
+	// 	panic(err)
+	// }
+
+	value, err := db.Get([]byte("name"))
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("value:", string(value))
+
+	err = db.Delete([]byte("name"))
+	if err != nil {
+		panic(err)
+	}
+}
--- options.go
+++ options.go
@@ -1,16 +1,26 @@
 package bitcask_go
 
+import "os"
+
 type Options struct {
 	DirPath      string // 数据目录
 	DataFileSize int64
-	SyncWrite    bool // 每次写入数据，持久化与否
-	IndexType    IndexType
+	SyncWrite    bool      // 每次写入数据，持久化与否
+	IndexType    IndexType // 所采用的索引类型
 }
 
 type IndexType = int8
 
+// 目前所能支持的索引类型
 const (
 	Btree IndexType = iota + 1
 	ART
 	BPlusTree
 )
+
+var DefaultOptions = Options{
+	DirPath:      os.TempDir(),
+	DataFileSize: 256 * 1024 * 1024,
+	SyncWrite:    false,
+	IndexType:    Btree,
+}
